ae94e053fda75af99656fd45e7e62dd56be5bc3c
felix18u <felix18u@etu.univ-lorraine.fr>
2018-10-09 15:49:14 +0200

    2eme commit avant 2eme cours

--- 07_forin.js
+++ 07_forin.js
@@ -7,7 +7,7 @@ describe ("For in", function() {
     };
 
     let a = "";
-    for(int i=0; i < obj.lenght(); i++) {                // set a for in loop, enumerating all property of obj in let p
+    for() {                // set a for in loop, enumerating all property of obj in let p
       p = obj[i];
       a += p;
     };
@@ -23,7 +23,7 @@ describe ("For in", function() {
     };
 
     let a = "";
-    for(int i=0; i < obj.lenght(); i++) {                // set a for in loop, enumerating all property of obj in let p
+    for(int i=0; i < obj.lenght i++) {                // set a for in loop, enumerating all property of obj in let p
       p = obj[i];
       a += p;
     };
@@ -37,7 +37,7 @@ describe ("For in", function() {
     let ar = [1, 2];
 
     let a = "";
-    for(int i=0; i < ar.lenght(); i++) {                // set a for in loop, enumerating all property of ar in let p
+    for(int i=0; i < ar.lenght; i++) {                // set a for in loop, enumerating all property of ar in let p
       a += ar[i];               // add the CONTENT of ar[p] to a
     };
 
@@ -50,7 +50,7 @@ describe ("For in", function() {
     ar.prop = 3;
 
     let a = "";
-    __ {                // set a for in loop, enumerating all property of ar in let p
+     {                // set a for in loop, enumerating all property of ar in let p
       __;               // add the CONTENT of ar[p] to a
     };
 
